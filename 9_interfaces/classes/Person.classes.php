18141105228f0eb646d2773f835c252074ee9be0
alfredkaziibwe19@gmail.com <alfredkaziibwe19@gmail.com>
2024-06-17 14:31:42 -0400

    interfaces

--- 9_interfaces/classes/Person.classes.php
+++ 9_interfaces/classes/Person.classes.php
@@ -1,6 +1,51 @@
 
 <?php
 
-class Paypal{
-    publi fucc
+interface PaymentInterface {
+
+    public function payNow();
 }
+
+class Paypal implements PaymentInterface{
+
+    public function loginFirst(){}
+    public function payNow(){}
+
+    public function paymentProcess(){
+        $this->loginFirst();
+        $this->payNow();
+
+    }
+}
+
+class Visa implements PaymentInterface {
+    public function payNow(){}
+    public function paymentProcess(){
+        $this->payNow();
+
+    }
+
+}
+
+class Cash implements PaymentInterface{
+
+    public function payNow(){}
+    public function paymentProcess(){
+        $this->payNow();
+
+    }
+
+}
+
+
+class BuyProduct 
+ {
+    public function pay(PaymentInterface $paymentType){
+
+$paymentType->paymentProcess();
+    }
+}
+
+$paymentType = new Cash();
+$buy = new BuyProduct();
+$buyProduct->pay($paymentType);
